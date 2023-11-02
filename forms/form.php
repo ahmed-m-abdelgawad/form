56ddd4689b0f14ff3c03589cfb1a4da5bc937626
Ahmed Mohamed Abdelgwad <47200062+ahmed-m-abdelgawad@users.noreply.github.com>
2023-11-02 13:53:25 +0200

    ok

--- forms/form.php
+++ forms/form.php
@@ -20,7 +20,7 @@ if (isset($_POST['search'])) {
   $search_code = $_POST['search_code'];
   
   // تحقق من صلاحية المستخدم للتعديل
-  if ($_SESSION['permission'] == 'له حق التعديل') {
+  if ($_SESSION['permission'] == 'ok') {
     // البحث عن العميل في جدول clients بناءً على كود العميل المدخل وكود الفرع الموجود في متغيرات جلسة
     $sql = "SELECT client_name, client_code FROM clients WHERE client_code = '$search_code' AND branch_code = '{$_SESSION['branch_code']}'";
     $result = $conn->query($sql);
