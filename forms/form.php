1bbd0c5c5b3b3dedfd134af9f7c92f9ad41b5142
Ahmed Mohamed Abdelgwad <47200062+ahmed-m-abdelgawad@users.noreply.github.com>
2023-11-02 13:13:41 +0200

    new

--- forms/form.php
+++ forms/form.php
@@ -8,7 +8,6 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "form_db";
-
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
@@ -20,7 +19,7 @@ if (isset($_POST['search'])) {
   $search_code = $_POST['search_code'];
   
   // تحقق من صلاحية المستخدم للتعديل
-  if ($_SESSION[`permission`] == 'له حق التعديل') {
+  if ($_SESSION['permission'] == 'له حق التعديل') {
     // البحث عن العميل في جدول clients بناءً على كود العميل المدخل وكود الفرع الموجود في متغيرات جلسة
     $sql = "SELECT client_name, client_code FROM clients WHERE client_code = '$search_code' AND branch_code = '{$_SESSION['branch_code']}'";
     $result = $conn->query($sql);
