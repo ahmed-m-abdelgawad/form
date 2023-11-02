47b6feff908bf576c87a4a6c21f16793ebd4bf04
Ahmed Mohamed Abdelgwad <47200062+ahmed-m-abdelgawad@users.noreply.github.com>
2023-11-02 16:40:08 +0200

    ok

--- forms/form.php
+++ forms/form.php
@@ -1,7 +1,9 @@
 // form.php
 <?php
 // بدء جلسة
-session_start();
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
 
 // اتصال بقاعدة بيانات MySQL باستخدام متغيرات جلسة
 $servername = "localhost";
@@ -15,120 +17,78 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// // // جلب بيانات العميل من قاعدة البيانات
-// if (!empty($_POST["search_code"])) {
-//    $search_code = $_POST["search_code"];
-
-//     // تحقق من صلاحية المستخدم للتعديل
-//     if ($_SESSION[`permission`] == 'له حق التعديل') {
-//         // البحث عن العميل في جدول clients بناءً على كود العميل المدخل وكود الفرع الموجود في متغيرات جلسة
-//         $sql = "SELECT client_name, client_code FROM clients WHERE client_code = '$search_code' AND branch_code = '{$_SESSION["branch_code"]}'";
-//         $result = $conn->query($sql);
-
-//         if ($result->num_rows > 0) {
-//             // جلب اسم وكود العميل
-//             $row = $result->fetch_assoc();
-//             $client_name = $row["client_name"];
-//             $client_code = $row["client_code"];
-//         } else {
-//             // لا يوجد عميل بهذا الكود أو ينتمي إلى فرع آخر
-//             $client_name = "";
-//             $client_code = "";
-//         }
-
-//         // جلب قائمة أكواد العملاء المتاحة للبحث من جدول clients بناءً على كود الفرع الموجود في متغيرات جلسة
-//         $sql = "SELECT client_code FROM clients WHERE status = 'not modified' AND branch_code = '{$_SESSION["branch_code"]}'";
-//         $result = $conn->query($sql);
-
-//         if ($result->num_rows > 0) {
-//             // إنشاء مصفوفة لحفظ أكواد العملاء
-//             $client_codes = [];
-//             while ($row = $result->fetch_assoc()) {
-//                 // إضافة كود العميل إلى المصفوفة
-//                 $client_codes[] = $row["client_code"];
-//             }
-//         } else {
-//             // لا يوجد عملاء غير معدّلين أو ينتمون إلى فرع آخر
-//             $client_codes = [];
-//         }
-//     } else {
-//         // ليس للمستخدم حق التعديل
-//         echo "<script>alert('ليس لديك صلاحية لتعديل بيانات العملاء');</script>";
-//     }
-// }
-
-
+// جلب بيانات العميل من قاعدة البيانات
 if (!empty($_POST["search_code"])) {
-  $search_code = $_POST["search_code"];
+    $search_code = $_POST["search_code"];
 
-   // Check if the user has permission to make modifications
-   if (isset($_SESSION["permission"]) && $_SESSION["permission"] == 'له حق التعديل') {
-       // Search for the client in the clients table based on the client code entered by the user and the branch code available in session variables
-       $sql = "SELECT client_name, client_code FROM clients WHERE client_code = '$search_code' AND branch_code = '{$_SESSION["branch_code"]}'";
-       $result = $conn->query($sql);
+    // تحقق من صلاحية المستخدم للتعديل
+    if (isset($_SESSION["permission"]) && $_SESSION["permission"] == 'له حق التعديل') {
+        // البحث عن العميل في جدول clients بناءً على كود العميل المدخل وكود الفرع الموجود في متغيرات جلسة
+        $sql = "SELECT client_name, client_code FROM clients WHERE client_code = '$search_code' AND branch_code = '{$_SESSION["branch_code"]}'";
+        $result = $conn->query($sql);
 
-       if (isset($result) && $result->num_rows > 0) {
-           // Retrieve client name and code
-           $row = $result->fetch_assoc();
-           $client_name = $row["client_name"];
-           $client_code = $row["client_code"];
-       } else {
-           // No client with this code exists or belongs to another branch
-           $client_name = "";
-           $client_code = "";
-       }
+        if (isset($result) && $result->num_rows > 0) {
+            // جلب اسم وكود العميل
+            $row = $result->fetch_assoc();
+            $client_name = $row["client_name"];
+            $client_code = $row["client_code"];
+        } else {
+            // لا يوجد عميل بهذا الكود أو ينتمي إلى فرع آخر
+            $client_name = "";
+            $client_code = "";
+        }
 
-       // Retrieve a list of client codes available for search from the clients table based on the branch code available in session variables
-       $sql = "SELECT client_code FROM clients WHERE status = 'not modified' AND branch_code = '{$_SESSION["branch_code"]}'";
-       $result = $conn->query($sql);
+        // جلب قائمة أكواد العملاء المتاحة للبحث من جدول clients بناءً على كود الفرع الموجود في متغيرات جلسة
+        $sql = "SELECT client_code FROM clients WHERE status = 'not modified' AND branch_code = '{$_SESSION["branch_code"]}'";
+        $result = $conn->query($sql);
 
-       if (isset($result) && $result->num_rows > 0) {
-           // Create an array to store client codes
-           $client_codes = array();
-           while ($row = $result->fetch_assoc()) {
-               // Add client code to the array
-               $client_codes[] = $row["client_code"];
-           }
-       } else {
-           // No unmodified clients exist or they belong to another branch
-           $client_codes = array();
-       }
-   } else {
-       // The user does not have permission to modify client data
-       echo "<script>alert('You do not have permission to modify client data');</script>";
-   }
+        if (isset($result) && $result->num_rows > 0) {
+            // إنشاء مصفوفة لحفظ أكواد العملاء
+            $client_codes = [];
+            while ($row = $result->fetch_assoc()) {
+                // إضافة كود العميل إلى المصفوفة
+                $client_codes[] = $row["client_code"];
+            }
+        } else {
+            // لا يوجد عملاء غير معدّلين أو ينتمون إلى فرع آخر
+            $client_codes = [];
+        }
+    } else {
+        // ليس للمستخدم حق التعديل
+        echo "<script>alert('ليس لديك صلاحية لتعديل بيانات العملاء');</script>";
+    }
 }
 
 // حفظ بيانات التعديل في قاعدة البيانات
 if (isset($_POST["submit"])) {
-  // استقبال بيانات التعديل
-  $client_name = $_POST["client_name"];
-  $client_code = $_POST["client_code"];
-  $coordinates = $_POST["coordinates"];
-  $notes = $_POST["notes"];
-  $date_time = date("Y-m-d H:i:s");
+    // استقبال بيانات التعديل
+    $client_name = $_POST["client_name"];
+    $client_code = $_POST["client_code"];
+    $coordinates = $_POST["coordinates"];
+    $notes = $_POST["notes"];
+    $date_time = date("Y-m-d H:i:s");
 
-  // تحقق من صلاحية المستخدم للتعديل
-  if ($_SESSION["permission"] == 'allowed to modify') {
-      // تحديث حالة العميل في جدول clients إلى modified
-      $sql = "UPDATE clients SET status = 'modified' WHERE client_code = '$client_code'";
-      if ($conn->query($sql) === true) {
-          echo "<script>alert('تم تحديث حالة العميل بنجاح');</script>";
-      } else {
-          echo "<script>alert('حدث خطأ أثناء تحديث حالة العميل');</script>";
-      }
+    // تحقق من صلاحية المستخدم للتعديل
+    if (isset($_SESSION["permission"]) && $_SESSION["permission"] == 'له حق التعديل') {
+        // تحديث حالة العميل في جدول clients إلى modified
+        $sql = "UPDATE clients SET status = 'modified' WHERE client_code = '$client_code'";
+        if ($conn->query($sql) === true) {
+            echo "<script>alert('تم تحديث حالة العميل بنجاح');</script>";
+        } else {
+            echo "<script>alert('حدث خطأ أثناء تحديث حالة العميل');</script>";
+        }
 
-      // إضافة بيانات التعديل في جدول modifications
-      $sql = "INSERT INTO modifications (client_code, client_name, coordinates, notes, date_time) VALUES ('$client_code', '$client_name', '$coordinates', '$notes', '$date_time')";
-      if ($conn->query($sql) === true) {
-          echo "<script>alert('تم إضافة بيانات التعديل بنجاح');</script>";
-      } else {
-          echo "<script>alert('حدث خطأ أثناء إضافة بيانات التعديل');</script>";
-      }
-  } else {
-      // ليس للمستخدم حق التعديل
-      echo "<script>alert('ليس لديك صلاحية لتعديل بيانات العملاء');</script>";
-  }
+        // إضافة بيانات التعديل في جدول modifications
+        $sql = "INSERT INTO modifications (client_code, client_name, coordinates, notes, date_time) VALUES ('$client_code', '$client_name', '$coordinates', '$notes', '$date_time')";
+        if ($conn->query($sql) === true) {
+            echo "<script>alert('تم إضافة بيانات التعديل بنجاح');</script>";
+        } else {
+            echo "<script>alert('حدث خطأ أثناء إضافة بيانات التعديل');</script>";
+        }
+    } else {
+        // ليس للمستخدم حق التعديل
+        echo "<script>alert('ليس لديك صلاحية لتعديل بيانات العملاء');</script>";
+    }
 }
 
 // إغلاق الاتصال بقاعدة البيانات
@@ -137,39 +97,39 @@ $conn->close();
 
 <html>
 <head>
-  <title>نموذج بيانات العملاء</title>
-  <!-- ربط ملف style.css -->
-  <link rel="stylesheet" href="../style.css">
+    <title>نموذج بيانات العملاء</title>
+    <!-- ربط ملف style.css -->
+    <link rel="stylesheet" href="../style.css">
 </head>
 <body>
-  <form method="post" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
     <h1>نموذج بيانات العملاء</h1>
     <p>ادخل كود العميل في الحقل التالي:</p>
     <input type="text" name="search_code" list="client_codes">
     <datalist id="client_codes">
-      <?php // عرض قائمة أكواد العملاء المتاحة
-      foreach ($client_codes as $code) {
-          echo "<option value='$code'>$code</option>";
-      } ?>
+        <?php // عرض قائمة أكواد العملاء المتاحة
+        foreach ($client_codes as $code) {
+            echo "<option value='$code'>$code</option>";
+        } ?>
     </datalist>
     <input type="submit" name="search" value="بحث">
     <p>اسم العميل:</p>
-    <input type="text" name="client_name" value="<?php echo $client_name; ?>" readonly>
+    <input type="text" name="client_name" value="<?php echo htmlspecialchars($client_name); ?>" readonly>
     <p>كود العميل:</p>
-    <input type="text" name="client_code" value="<?php echo $client_code; ?>" readonly>
+    <input type="text" name="client_code" value="<?php echo htmlspecialchars($client_code); ?>" readonly>
     <p>كود الفرع:</p>
-    <input type="text" name="branch_code" value="<?php echo $_SESSION[
+    <input type="text" name="branch_code" value="<?php echo htmlspecialchars($_SESSION[
         "branch_code"
-    ]; ?>" readonly>
+    ]); ?>" readonly>
     <p>إحداثيات الموقع:</p>
     <input type="text" name="coordinates" id="coordinates" readonly>
     <p>الملاحظات:</p>
     <textarea name="notes" rows="5"></textarea>
     <input type="submit" name="submit" value="إرسال">
-  </form>
+</form>
 
-  <!-- ربط ملف script.js -->
-  <script src="../script.js"></script>
+<!-- ربط ملف script.js -->
+<script src="../script.js"></script>
 </body>
 </html>
 
